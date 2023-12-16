6c5bd21e7fa902b55be0767d43356da1a460dabf
ynitishreddy <yasanitishreddy@gmail.com>
2023-12-16 21:41:55 +0530

    new

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,42 +1,49 @@
 /* eslint-disable no-undef */
 const todoList = () => {
-    all = []
-    const add = (todoItem) => {
-        all.push(todoItem)
-    }
-    const markAsComplete = (index) => {
-        all[index].completed = true
-    }
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
 
-    const overdue = () => {
-        const today = new Date().toISOString().split("T")[0];
-        return all.filter((todoitem) => todoitem.dueDate < today);
-    }
+  const overdue = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((todoitem) => todoitem.dueDate < today);
+  };
 
-    const dueToday = () => {
-        const today = new Date().toISOString().split("T")[0];
-        return all.filter((todoitem) => todoitem.dueDate === today);
-    }
+  const dueToday = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((todoitem) => todoitem.dueDate === today);
+  };
 
-    const dueLater = () => {
-        const today = new Date().toISOString().split("T")[0];
-        return all.filter((todoitem) => todoitem.dueDate > today);
-    }
+  const dueLater = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((todoitem) => todoitem.dueDate > today);
+  };
 
-    const toDisplayableList = (todos) => {
-        const today = new Date().toISOString().split("T")[0];
-        return todos.map((item, index) => `[${item.completed ? 'x' : ' '}] ${item.title} ${item.dueDate === today ? '' : item.dueDate}`).join('\n');
-    }
+  const toDisplayableList = (todos) => {
+    const today = new Date().toISOString().split("T")[0];
+    return todos
+      .map(
+        (item) =>
+          `[${item.completed ? "x" : " "}] ${item.title} ${
+            item.dueDate === today ? "" : item.dueDate
+          }`,
+      )
+      .join("\n");
+  };
 
-    return {
-        all,
-        add,
-        markAsComplete,
-        overdue,
-        dueToday,
-        dueLater,
-        toDisplayableList
-    };
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
 };
 
 // ####################################### #
