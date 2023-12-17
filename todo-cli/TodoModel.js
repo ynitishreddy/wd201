23d4168979d5ff7b2f0bc50394bc12e3575440c7
ynitishreddy <yasanitishreddy@gmail.com>
2023-12-17 22:07:31 +0530

    new commit

---
+++ todo-cli/TodoModel.js
@@ -0,0 +1,25 @@
+const { DataTypes, Model } = require("sequelize");
+const { sequelize } = require("./connectDB.js");
+
+class Todo extends Model {}
+
+Todo.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dueDate: {
+      type: DataTypes.DATEONLY,
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    sequelize,
+  },
+);
+
+module.exports = Todo;
+Todo.sync();
